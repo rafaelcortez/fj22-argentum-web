a27c389e8ca3d6afbb4b75f1979042a47ad1af8d
Rafael Cortez Di Giulio <rafa.digiulio@gmail.com>
2014-12-18 21:02:37 -0200

    Quinta feira

---
+++ src/main/java/OlaMundoBean.java
@@ -0,0 +1,25 @@
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class OlaMundoBean {
+
+	private String mensagem = "Quem é você?";
+	private String nome;
+	
+	public String getMensagem() {
+		return mensagem;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+		System.out.println("\nChambotao");
+	}
+	
+	public void nomeFoiDigitado(){
+		System.out.println("\nChamou o botao");
+	}
+}
